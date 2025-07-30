c15d161e61435d8c858566340729acd4c957ac0d
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-07-29 22:56:54 -0500

    primer nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponentComponent } from "./components/footer-component/footer-component.component";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponentComponent],
+  imports: [RouterOutlet, FooterComponentComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
