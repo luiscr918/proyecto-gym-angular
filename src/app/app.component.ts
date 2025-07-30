10311b40eee5c71d1166ea5dd8583d5519c1125a
luiscr918 <lc9107040@gmail.com>
2025-07-29 20:56:50 -0500

    alisson el tailwind estaba mal puesto jaja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponentComponent } from "./components/footer-component/footer-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
