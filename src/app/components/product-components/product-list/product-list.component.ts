b7597d65043b70f24411a9093587fa2b0db0e1f4
luiscr918 <lc9107040@gmail.com>
2025-07-31 16:45:31 -0500

    pagina productos lista

--- src/app/components/product-components/product-list/product-list.component.ts
+++ src/app/components/product-components/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
-import {  ProductoConId } from '../../../interfaces/productos';
+import { ProductoConId } from '../../../interfaces/productos';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
@@ -16,7 +16,47 @@ export class ProductListComponent {
   ngOnInit() {
     this.servicioProducto.getProducts().subscribe((data) => {
       this.productos = data;
-      console.log("Productos con ID:", this.productos);
+      console.log('Productos con ID:', this.productos);
     });
+  }
+  // codigo para animacion de el titulo
+
+  ngAfterViewInit(): void {
+    // El código del script se coloca aquí dentro
+    const words = [
+      'Descubre los productos fitness',
+      'Suplementos de alta calidad',
+      '¡Entrena al máximo!',
+    ];
+    let i = 0;
+    let j = 0;
+    let currentWord = '';
+    let isDeleting = false;
+
+    const type = () => {
+      currentWord = words[i];
+      if (isDeleting) {
+        document.getElementById('typewriter')!.textContent =
+          currentWord.substring(0, j - 1);
+        j--;
+        if (j === 0) {
+          isDeleting = false;
+          i++;
+          if (i === words.length) {
+            i = 0;
+          }
+        }
+      } else {
+        document.getElementById('typewriter')!.textContent =
+          currentWord.substring(0, j + 1);
+        j++;
+        if (j === currentWord.length) {
+          isDeleting = true;
+        }
+      }
+      setTimeout(type, 100);
+    };
+
+    type();
   }
 }
