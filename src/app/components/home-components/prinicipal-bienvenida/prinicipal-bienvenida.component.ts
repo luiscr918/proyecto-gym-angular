1cb5da85692a8c27da487a17c633a11632054960
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-07-31 10:25:23 -0500

    home, algo mas o menos hecho, pero feo

---
+++ src/app/components/home-components/prinicipal-bienvenida/prinicipal-bienvenida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prinicipal-bienvenida',
+  standalone: true,
+  imports: [],
+  templateUrl: './prinicipal-bienvenida.component.html',
+  styleUrl: './prinicipal-bienvenida.component.css'
+})
+export class PrinicipalBienvenidaComponent {
+
+}
