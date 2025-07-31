072e32918fa60ab435730af8bb73e0d731af592c
luiscr918 <lc9107040@gmail.com>
2025-07-30 21:37:59 -0500

    plantillas listas para consumir la api de firebase

--- src/app/components/product-components/product-list/product-list.component.ts
+++ src/app/components/product-components/product-list/product-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../nav-bar/nav-bar.component";
+import { FooterComponentComponent } from "../../footer-component/footer-component.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, FooterComponentComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProductDatailsComponent } from "../../components/product-components/product-datails/product-datails.component";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { FooterComponentComponent } from "../../components/footer-component/footer-component.component";
+import { ProductListComponent } from "../../components/product-components/product-list/product-list.component";
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [ProductDatailsComponent, NavBarComponent, FooterComponentComponent],
+  imports: [ProductDatailsComponent, NavBarComponent, FooterComponentComponent, ProductListComponent],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
