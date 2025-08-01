2877cd0f56c730d424a0069ead5950c9ce13bbe7
luiscr918 <lc9107040@gmail.com>
2025-08-01 07:29:48 -0500

    terminado taller 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { ProductDatailsComponent } from './components/product-components/product
 import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
 import { RegistroProductoComponent } from './components/product-components/registro-producto/registro-producto.component';
 import { ProductActualizarComponent } from './components/product-components/product-actualizar/product-actualizar.component';
+import { NosotrosPageComponent } from './pages/nosotros-page/nosotros-page.component';
+import { SuscripcionPageComponent } from './pages/suscripcion-page/suscripcion-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -13,6 +15,7 @@ export const routes: Routes = [
   { path: 'productos/:id', component: ProductDatailsComponent },
   { path: 'contactanos', component: ContactUsPageComponent },
   { path: 'agregar-producto', component: RegistroProductoComponent },
-
+  { path: 'nosotros', component: NosotrosPageComponent },
+  { path: 'suscripcion', component: SuscripcionPageComponent },
   { path: 'products/:id', component: ProductActualizarComponent },
 ];
